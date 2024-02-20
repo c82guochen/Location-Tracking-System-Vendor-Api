daf6502d28cba154b47962bc8ca81f46787cb8bd
Chen Guo <c82guo@uwaterloo.ca>
2024-02-20 01:46:37 -0500

    test16

--- src/disconnect.ts
+++ src/disconnect.ts
@@ -1,40 +1,7 @@
-// import { APIGatewayEvent, APIGatewayProxyResult } from 'aws-lambda';
-// import { dynamoDbRemoveConnection } from './aws';
-
-// // 和connect中lambda函数名一样没关系，在执行docker命令的时候标清楚不一样就行了
-// export const handler = async (
-//   event: APIGatewayEvent
-// ): Promise<APIGatewayProxyResult> => {
-//   const tableName =
-//     process.env.AWS_TABLE_NAME ?? 'websocket-connections';
-//   const connectionId = event.requestContext.connectionId ?? '';
-//   console.log('attempt user:', connectionId);
-
-//   const res = await dynamoDbRemoveConnection(tableName, connectionId);
-//   if (res instanceof Error) {
-//     console.log('error', res.message);
-//     return {
-//       statusCode: 500,
-//       headers: {
-//         'content-type': 'text/plain; charset=utf-8',
-//       },
-//       body: res.message,
-//     };
-//   }
-
-//   console.log('disconnected');
-
-//   return {
-//     statusCode: 200,
-//     body: JSON.stringify({
-//       message: `User ${connectionId} disconnected!`,
-//     }),
-//   };
-// };
-
 import { APIGatewayEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { dynamoDbRemoveConnection } from './aws';
 
+// 和connect中lambda函数名一样没关系，在执行docker命令的时候标清楚不一样就行了
 export const handler = async (
   event: APIGatewayEvent
 ): Promise<APIGatewayProxyResult> => {
--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,108 +1,9 @@
-# # Connect
-# resource "aws_lambda_function" "connect" {
-#   # 注意：配置的是名为connect的docker image
-#   function_name = "${var.app_name}-connect"
-#   role = aws_iam_role.lambda_main.arn
-#   # 这里的url是指在docker存到ecr中之后的image url（image_tag是被build出来的）
-#   image_uri = "${local.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/connect:${var.image_tag}"
-#   package_type = "Image"
-#   timeout = 30
-#   environment {
-#     variables = {
-#         AWS_TABLE_NAME = "${var.websocket_table_name}"
-#     }
-#   }
-# }
-
-# # 这里是关于resources的permission  
-# resource "aws_lambda_permission" "connect_permission" {
-#     # 有一些内容是默认的
-#     statement_id = "AllowExecutionFromAPIGateway"
-#     action = "lambda:InvokeFunction"
-#     function_name = aws_lambda_function.connect.function_name
-#     principal = "apigateway.amazonaws.com"
-#     source_arn = "${aws_apigatewayv2_api.websocket_cg.execution_arn}/*/*"
-# }
-
-# # Disconnect
-# resource "aws_lambda_function" "disconnect" {
-#   function_name = "${var.app_name}-disconnect"
-#   role = aws_iam_role.lambda_main.arn
-#   image_uri = "${local.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/disconnect:${var.image_tag}"
-#   package_type = "Image"
-#   timeout = 30
-#   environment {
-#     variables = {
-#         AWS_TABLE_NAME = "${var.websocket_table_name}"
-#     }
-#   }
-# }
-
-# resource "aws_lambda_permission" "disconnect_permission" {
-#     statement_id = "AllowExecutionFromAPIGateway"
-#     action = "lambda:InvokeFunction"
-#     function_name = aws_lambda_function.disconnect.function_name
-#     principal = "apigateway.amazonaws.com"
-#     source_arn = "${aws_apigatewayv2_api.websocket_cg.execution_arn}/*/*"
-# }
-
-# # sendvendor
-# resource "aws_lambda_function" "sendvendor" {
-#   function_name = "${var.app_name}-sendvendor"
-#   role = aws_iam_role.lambda_main.arn
-#   image_uri = "${local.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/sendvendor:${var.image_tag}"
-#   package_type = "Image"
-#   timeout = 30
-#   environment {
-#     variables = {
-#         AWS_TABLE_NAME = "${var.websocket_table_name}"
-#         AWS_SQS_URL = "https://sqs.us-east-1.amazonaws.com/656203730697/vendor-twitter-queue"
-#         # 因为需要在web socket里broadcast，所以需要web socket url
-#         AWS_WEBSOCKET_URL = "${aws_apigatewayv2_api.websocket_cg.api_endpoint}/${var.api_gateway_stage_name}"
-#     }
-#   }
-# }
-
-# resource "aws_lambda_event_source_mapping" "sqs_trigger" {
-#     # trigger的对象
-#     event_source_arn = "arn:aws:sqs:${var.aws_region}:${local.account_id}:${var.sqs_queue_name}"
-#     function_name = aws_lambda_function.sendvendor.arn
-# }
-
-# resource "aws_lambda_permission" "sendvendor_permission" {
-#     statement_id = "AllowExecutionFromSQS"
-#     action = "lambda:InvokeFunction"
-#     function_name = aws_lambda_function.sendvendor.function_name
-#     principal = "sqs.amazonaws.com"
-#     source_arn = "arn:aws:sqs:${var.aws_region}:${local.account_id}:${var.sqs_queue_name}"
-# }
-
-# # getvendors
-# resource "aws_lambda_function" "getvendors" {
-#   function_name = "${var.app_name}-getvendors"
-#   role = aws_iam_role.lambda_main.arn
-#   image_uri = "${local.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/getvendors:${var.image_tag}"
-#   package_type = "Image"
-#   timeout = 30
-#   environment {
-#     variables = {
-#         AWS_VENDOR_TABLE_NAME = "${var.vendor_table_name}"
-#     }
-#   }
-# }
-
-# resource "aws_lambda_permission" "getvendors_permission" {
-#     statement_id = "AllowExecutionFromAPIGateway"
-#     action = "lambda:InvokeFunction"
-#     function_name = aws_lambda_function.getvendors.function_name
-#     principal = "apigateway.amazonaws.com"
-#     source_arn = "${aws_apigatewayv2_api.http_cg.execution_arn}/*/*"
-# }
-
 # Connect
 resource "aws_lambda_function" "connect" {
+  # 注意：配置的是名为connect的docker image
   function_name = "${var.app_name}-connect"
   role = aws_iam_role.lambda_main.arn
+  # 这里的url是指在docker存到ecr中之后的image url（image_tag是被build出来的）
   image_uri = "${local.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/connect:${var.image_tag}"
   package_type = "Image"
   timeout = 30
@@ -113,12 +14,14 @@ resource "aws_lambda_function" "connect" {
   }
 }
 
+# 这里是关于resources的permission  
 resource "aws_lambda_permission" "connect_permission" {
+    # 有一些内容是默认的
     statement_id = "AllowExecutionFromAPIGateway"
     action = "lambda:InvokeFunction"
     function_name = aws_lambda_function.connect.function_name
     principal = "apigateway.amazonaws.com"
-    source_arn = "${aws_apigatewayv2_api.websocket_gw.execution_arn}/*/*"
+    source_arn = "${aws_apigatewayv2_api.websocket_cg.execution_arn}/*/*"
 }
 
 # Disconnect
@@ -140,7 +43,7 @@ resource "aws_lambda_permission" "disconnect_permission" {
     action = "lambda:InvokeFunction"
     function_name = aws_lambda_function.disconnect.function_name
     principal = "apigateway.amazonaws.com"
-    source_arn = "${aws_apigatewayv2_api.websocket_gw.execution_arn}/*/*"
+    source_arn = "${aws_apigatewayv2_api.websocket_cg.execution_arn}/*/*"
 }
 
 # sendvendor
@@ -154,12 +57,14 @@ resource "aws_lambda_function" "sendvendor" {
     variables = {
         AWS_TABLE_NAME = "${var.websocket_table_name}"
         AWS_SQS_URL = "https://sqs.us-east-1.amazonaws.com/656203730697/vendor-twitter-queue"
-        AWS_WEBSOCKET_URL = "${aws_apigatewayv2_api.websocket_gw.api_endpoint}/${var.api_gateway_stage_name}"
+        # 因为需要在web socket里broadcast，所以需要web socket url
+        AWS_WEBSOCKET_URL = "${aws_apigatewayv2_api.websocket_cg.api_endpoint}/${var.api_gateway_stage_name}"
     }
   }
 }
 
 resource "aws_lambda_event_source_mapping" "sqs_trigger" {
+    # trigger的对象
     event_source_arn = "arn:aws:sqs:${var.aws_region}:${local.account_id}:${var.sqs_queue_name}"
     function_name = aws_lambda_function.sendvendor.arn
 }
@@ -169,7 +74,7 @@ resource "aws_lambda_permission" "sendvendor_permission" {
     action = "lambda:InvokeFunction"
     function_name = aws_lambda_function.sendvendor.function_name
     principal = "sqs.amazonaws.com"
-     source_arn = "arn:aws:sqs:${var.aws_region}:${local.account_id}:${var.sqs_queue_name}"
+    source_arn = "arn:aws:sqs:${var.aws_region}:${local.account_id}:${var.sqs_queue_name}"
 }
 
 # getvendors
@@ -191,5 +96,6 @@ resource "aws_lambda_permission" "getvendors_permission" {
     action = "lambda:InvokeFunction"
     function_name = aws_lambda_function.getvendors.function_name
     principal = "apigateway.amazonaws.com"
-    source_arn = "${aws_apigatewayv2_api.http_gw.execution_arn}/*/*"
+    source_arn = "${aws_apigatewayv2_api.http_cg.execution_arn}/*/*"
 }
+
